0b18daf82bd6a4977d9270888e7f0e8e87fee7aa
Hamza <hamzabilalbaig@gmail.com>
2022-12-14 12:42:25 +0500

    room connection working

--- Screens/Rooms.jsx
+++ Screens/Rooms.jsx
@@ -5,19 +5,28 @@ import {
   Platform,
   StyleSheet,
   SafeAreaView,
+  TextInput,
 } from "react-native";
 import React from "react";
 import { useNavigation } from "@react-navigation/native";
 import { colors } from "../constants/Colors";
+import { useRef } from "react";
+import { useEffect } from "react";
+import io from "socket.io-client";
 
 const Rooms = () => {
+  const socket = useRef();
+
+  useEffect(() => {
+    socket.current = io("ws://192.168.100.241:8001");
+    socket.current.emit("join-room", { roomId: "1", emailId: "any@rmaa.com" });
+  }, []);
   const navigation = useNavigation();
   return (
     <View style={styles.container}>
       <SafeAreaView>
-        <Text onPress={() => navigation.navigate("Rooms")}>
-          Rooms Will be in the final version
-        </Text>
+        <Text onPress={() => console.log("Rooms")}>Enter Room Code</Text>
+        <TextInput placeholder="3213214" />
       </SafeAreaView>
     </View>
   );
